89f1f3dd43dbd7a6d0b7b0e17c9199176f43e8b4
Alex Jackson <alexx.j.jackson@gmail.com>
2022-09-30 15:20:33 -0700

    move modal to components folder, test modal

--- react-front-end/src/components/Modal.js
+++ react-front-end/src/components/Modal.js
@@ -5,14 +5,14 @@ function Modal() {
 // make sure to add state to this component, related to each of the cards on pages
 
   return (
-    <div className="modal-background">
+    
       <div className="modal">
         <button className="close-button">X</button>
         <div className="title">
           <h1><b>Recipe Title</b></h1>
         </div>
         <div className="modal-image">
-          <img src="https://preview.redd.it/b7zpw9mk84c51.jpg?auto=webp&s=2d64e494a8ba795d35d8cd4b4afd9faf7e6bc4e9" />
+          <img src="https://preview.redd.it/b7zpw9mk84c51.jpg?auto=webp&s=2d64e494a8ba795d35d8cd4b4afd9faf7e6bc4e9" alt="cocktail"/>
         </div>
         <div className="modal-ingredients">
           <h3><b>Ingredients</b></h3>
@@ -23,7 +23,6 @@ function Modal() {
           <p>There will be instructions here, taken from the API</p>
         </div>
       </div>
-    </div>
   );
 }
 
