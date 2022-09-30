3eb085a370dd210004e6d3d83984192a4d92a14f
mustafa <mustafa.bhm3@gmail.com>
2022-09-30 08:22:37 -0600

    adding frontend 7 backend folders

---
+++ react-front-end/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2> Hello </h2>
+    </div>
+  );
+}
+
+export default App;
