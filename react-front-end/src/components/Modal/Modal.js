e15e68fbedeb259df3d5bd43090dd50cfec3c6e0
Alexander Jackson <alexx.j.jackson@gmail.com>
2022-09-30 19:39:11 +0000

    add modal skeleton

---
+++ react-front-end/src/components/Modal/Modal.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Modal() {
+
+  return (
+    <div className="modal-background">
+      <div className="modal">
+        <button className="close-button">X</button>
+        <div className="title">
+          <h1><b>Recipe Title</b></h1>
+        </div>
+        <div className="modal-image">
+          <img src="https://preview.redd.it/b7zpw9mk84c51.jpg?auto=webp&s=2d64e494a8ba795d35d8cd4b4afd9faf7e6bc4e9" />
+        </div>
+        <div className="modal-ingredients">
+          <h3><b>Ingredients</b></h3>
+          <p>There will be an ordered list here using the 'ul' tag</p>
+        </div>
+        <div className="modal-instructions">
+          <h3><b>Instructions</b></h3>
+          <p>There will be instructions here, taken from the API</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
