8af43e2a749d9a0b64e12399bf6dd2226d03eb58
mustafa <mustafa.bhm3@gmail.com>
2022-09-30 08:39:58 -0600

    add navbar

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,12 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 import axios from "axios";
 import "./App.css";
+import NavigationBar from "./components/NavigationBar";
 import "bootstrap/dist/css/bootstrap.min.css";
+import MaybeHome from "./components/MaybeHome";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h2> Hello </h2>
+      <Router>
+        <NavigationBar />
+        <Routes>
+          <Route exact path="/" element={<MaybeHome />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/Login" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
