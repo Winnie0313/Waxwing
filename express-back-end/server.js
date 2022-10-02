3770bcc5326b19e4543cdc7f380ec73ad28cf36a
Alex <alexx.j.jackson@gmail.com>
2022-10-01 19:52:42 -0700

    work on knex routing

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -3,8 +3,8 @@ const App = Express();
 const BodyParser = require('body-parser');
 const { application } = require('express');
 const PORT = 8001;
-const knexConfig = require('./knexfile');
-const knex = require('knex')(knexConfig);
+// const knexConfig = require('./knexfile');
+// const knex = require('knex')(knexConfig);
 
 // Express Configuration
 App.use(BodyParser.urlencoded({ extended: false }));
@@ -26,8 +26,8 @@ const favouritesRoutes = require('./routes/favourites');
 
 // API Routes
 
-App.use('/api/users', usersRoutes(knex));
-App.use('/api/favourites', favouritesRoutes(knex));
+// App.use('/api/users', usersRoutes(knex));
+// App.use('/api/favourites', favouritesRoutes(knex));
 
 // Server Activation
 
