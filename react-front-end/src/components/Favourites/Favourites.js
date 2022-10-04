abf5fc5f2fba695d15e7086a7694785b1856deee
Alex <alexx.j.jackson@gmail.com>
2022-10-04 13:36:17 -0700

    begin work on favourites page, fetching database data

--- react-front-end/src/components/Favourites/Favourites.js
+++ react-front-end/src/components/Favourites/Favourites.js
@@ -1,12 +1,40 @@
-import react from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { UserContext } from '../UserContext';
 
 
 const Favourites = () => {
+
+    const { user, setUser } = useContext(UserContext);
+
+    const [db, setDb] = useState([]);
+
+    const drinkArray = [];
+
+    useEffect(() => {
+        fetchFavourites();
+    }, []);
+
+    const fetchFavourites = async () => {
+        const response = await fetch('localhost:8001/api/favourites/1');
+        const data = await response.json();
+        console.log(data);
+        setDb();
+        console.log(db);
+    };
+
+    
     return (
-      
+
+        
+            
         <div>
             <h1>Favourites</h1>
         </div>
+
+
+
+        
+      
     )
 }
 
