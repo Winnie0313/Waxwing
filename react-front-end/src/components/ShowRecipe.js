8991541a2261b49c68a8505cb3b0d4a2f69cc0f8
Winnie0313 <liwanyi0313@gmail.com>
2022-10-05 10:49:44 -0700

    fixed styles

--- react-front-end/src/components/ShowRecipe.js
+++ react-front-end/src/components/ShowRecipe.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+
 const axios = require("axios");
 
 
@@ -7,6 +10,7 @@ function ShowRecipe() {
   const [drink, setDrink] = useState({});
   const [ingredients, setIngredients] = useState([])
   const [measurements, setMeasurements] = useState([])
+  // get drink id from the endpoint
   const { id } = useParams();
   console.log(id);
 
@@ -52,18 +56,90 @@ function ShowRecipe() {
     setMeasurements(measurementsArray);
   };
 
-  // const ingredients = ingredientsForDrink();
-  // const measurements = measurementsForDrink();
-  // console.log("Ingredients are: ", ingredients);
-  // console.log("measurements are: ", measurements);
-
-
-
   return (
-    <p>Recipe</p>
+    <div>
+      <GridContainer>
+        <TopLeft>
+          <div>
+            <h1>{drink.strDrink}</h1>
+            <p>{drink.strCategory}</p>
+          </div>
+        </TopLeft>
+        <TopRight>
+          <img src={drink.strDrinkThumb} alt={drink.strDrink} />
+        </TopRight> 
+      
+    
+        <BottomLeft>
+          <h3>Ingredients</h3>
+          <div>
+            <ul>
+              {ingredients.map((ingredient, index) => {
+                return (
+                  <li key={index}>
+                    {ingredient} - {measurements[index]}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </BottomLeft>
+        <BottomRight>
+          <h3>Instructions</h3>
+          <p>{drink.strInstructions}</p>
+        </BottomRight>
+      </GridContainer>
+    </div>
+
 
   );
 
 }
+
+
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-rows: repeat(2, 1fr);
+  grid-template-columns: repeat(2, 1fr);
+  width: 100vw;
+  height: 100vh;
+`
+
+// const Top = styled.div`
+//   display: flex;
+// `
+const TopLeft = styled.div`
+  display: grid;
+  align-items: center;
+  flex: 50%;
+  padding-left: 1.5rem;
+  padding-right: 1.5rem;
+  
+  h1 {
+    font-size: 50px;
+  }
+`
+const TopRight = styled.div`
+  flex: 50%;
+  img {
+    width: 100%;
+  };
+`
+const BottomLeft = styled.div`
+  padding: 1.5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  li {
+    text-align: left;
+  }
+`
+const BottomRight = styled.div`
+  padding: 1.5rem;
+  display: flex;
+  flex-direction: column;
+  
+`
+
 
 export default ShowRecipe;
